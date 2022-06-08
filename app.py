76ed133eb5c78b4390185733154d35e9b40a6437
Naina <66205648+naina8400@users.noreply.github.com>
2022-06-08 23:45:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
+from PIL import Image
+from datetime import datetime
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # hide_menu_style = """
 #         <style>
@@ -35,6 +39,15 @@ def load_data(nrows):
 
 data = load_data(100000)
 original_data = data
+dataset_old1=load_data(100000)
+dataset_old=pd.DataFrame(dataset_old1.iloc[:,1:22])
+
+###Adding the Category column###
+
+
+dat_cat = pd.DataFrame(dataset_old.iloc[:,10:16].idxmax(axis=1,skipna=True),columns=['category'])
+col = [dataset_old,dat_cat]
+dataset = pd.concat(col,axis=1)
 
 st.header("Where are the most people injured in NYC?")
 injured_people = st.slider("Number of persons injured in collision  💥 🚗", 0, 19)
@@ -79,6 +92,57 @@ chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
 fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
 st.write(fig)
 
+st.header(" Area and Category wise Analysis ") 
+st.write("*This section will help you understand the specific areas of NYC with" 
+    "the most number of accidents in the entire dataset along with category wise distribution.*")
+pidata1 = dataset['borough'].value_counts()
+pie_frame1 = pd.Series.to_frame(pidata1)
+pie_frame1['Name'] = list(pie_frame1.index)
+    #st.write(pie_frame1)
+fig = px.pie(pie_frame1, values="borough",names="Name", title="Long-Form Input")
+    #st.write(fig)
+pidata2 =dataset['category'].value_counts()
+pie_frame2 =pd.Series.to_frame(pidata2)
+pie_frame2['Name']=list(pie_frame2.index)
+    #st.write(pie_frame2)
+fig = go.Figure()
+fig = make_subplots(rows=1, cols=3, specs=[[{'type':'domain'}, {'type':'domain'},  {'type':'domain'}]],
+            subplot_titles=['Area Wise Count',' ','Category Wise Count'])
+
+fig.add_trace(go.Pie(labels=pie_frame1['Name'], values=pie_frame1['borough'], name="Area Wise Count",showlegend=False,
+            textinfo='label+percent',insidetextorientation='radial',
+            ),1, 1)
+
+fig.add_trace(go.Pie(labels=pie_frame2['Name'], values=pie_frame2['category'], name="Category Wise Count",
+            showlegend=False,textinfo='label+percent',insidetextorientation='radial',
+            ),1, 3)
+
+fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+fig.update_traces(hole=.4, hoverinfo="label+percent+name")
+fig.update_layout(
+    autosize=False,
+    width=850,
+    height=400,
+    margin=dict(
+        l=0,
+        r=0,
+        b=0,
+        t=0,
+       
+    ))
+st.write(fig)
+
+
+### BAR GRAPH FOR DANGEORUS STREETS ###
+    
+
+street_count=data["on_street_name"].value_counts()
+street_datafrm=pd.Series.to_frame(street_count) 
+street_datafrm['id']=list(street_datafrm.index)
+fig = px.histogram(street_datafrm.iloc[0:20,:],x="id",y="on_street_name",height=500,width=900,)
+st.write(fig)
+
+
 st.header("Top 5 dangerous streets by affected type")
 select = st.selectbox('Affected type of people', ['Pedestrians', 'Cyclists', 'Motorists'])
 
@@ -95,3 +159,4 @@ else:
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
     st.write(data)
+
